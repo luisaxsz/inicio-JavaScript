1356c9c8b476fbb626e1c676e93a325eecb1a72f
Maria Luisa Lameirão Sousa <101007311+luisaxsz@users.noreply.github.com>
2022-12-24 13:36:24 -0300

    Create BoasVindas.js

---
+++ DesafioBoasvindas/DesafioBoasVindas001/BoasVindas.js
@@ -0,0 +1,7 @@
+function inicio() {
+    let name = window.prompt('Qual seu nome?')
+    let idade = window.prompt(`Olá ${name}! Quantos anos você tem?`)
+    let alert = window.alert(
+      `Acabei de conhecer a ${name}, que tem ${idade} de idade!`
+    )
+  }
