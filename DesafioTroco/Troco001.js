0a0f268822e66ede7bd2bd1662f000442d353fb1
Maria Luisa Lameirão Sousa <101007311+luisaxsz@users.noreply.github.com>
2022-12-24 13:22:59 -0300

    Delete Troco001.js

--- DesafioTroco/Troco001.js
+++
@@ -1,9 +0,0 @@
-       function troco() {
-        let obj = window.prompt('Oque você comprou?')
-        let valor = window.prompt(`Qual foi o valor do seu ${obj}`)
-        let pag = window.prompt('Qual valor você vai usar para pagar?')
-        let troco = pag - valor
-        let res = alert(
-          `Você comprou um(a) ${obj} que custou ${valor} e deu o valor de  ${pag} seu troco é de ${troco}`
-        )
-      }
